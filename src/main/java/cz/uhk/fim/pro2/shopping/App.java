09d71b48f014ad1fd18ee8ed36f55258ef7cbc58
Kkotva <davidcastka97@gmail.com>
2020-10-19 15:43:27 +0200

    0.0.2

--- src/main/java/cz/uhk/fim/pro2/shopping/App.java
+++ src/main/java/cz/uhk/fim/pro2/shopping/App.java
@@ -1,9 +1,23 @@
 package cz.uhk.fim.pro2.shopping;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
 
     public static void main(String[] args) {
-
+    launch(args);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/text.fxml"));
+        stage.setScene(new Scene(root, 640,480));
+        stage.show();
+    }
 }
---
+++ src/main/java/cz/uhk/fim/pro2/shopping/ui/TestController.java
@@ -0,0 +1,8 @@
+package cz.uhk.fim.pro2.shopping.ui;
+
+public class TestController {
+
+    public void onMouseClick(){
+        System.out.println("Hello world");
+    }
+}
